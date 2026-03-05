a97aff04e08a88b6d938ba0975c5793584f3a8b0
Muhammad Andre Widianto <andrewidianto12@gmail.com>
2026-03-05 21:10:11 +0700

    update go mod

---
+++ app/rental-manufacture/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/andrewidianto12/Manufacture-Rental/database"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	loadEnv()
+
+	db := database.InitDatabase()
+	defer db.Close()
+
+	addr := os.Getenv("APP_HOST") + ":" + os.Getenv("APP_PORT")
+	fmt.Println("Database connection test successful")
+	log.Default().Println("Application initialized on " + addr)
+}
+
+func loadEnv() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Println("Warning: .env file not found, menggunakan sistem env")
+	}
+}
